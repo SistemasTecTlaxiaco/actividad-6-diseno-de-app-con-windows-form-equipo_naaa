78308292f9877f6133b2b0d1671872d737a35060
nallely-lopez <lopezgarcializethnallely@gmail.com>
2025-03-20 16:57:20 -0600

    Se implementaron los componentes perzonalizados, en este caso textbox y boton

--- AppAgenda/Interfaz_Inicio.Designer.cs
+++ AppAgenda/Interfaz_Inicio.Designer.cs
@@ -29,15 +29,36 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            rjButton1 = new CustomControls.RJControls.RJButton();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.DarkSlateGray;
+            panel1.Controls.Add(rjButton1);
             panel1.Location = new Point(-11, -6);
             panel1.Name = "panel1";
             panel1.Size = new Size(1500, 1057);
             panel1.TabIndex = 1;
+            // 
+            // rjButton1
+            // 
+            rjButton1.BackColor = Color.CadetBlue;
+            rjButton1.BackgroundColor = Color.CadetBlue;
+            rjButton1.BorderColor = Color.SlateGray;
+            rjButton1.BorderRadius = 60;
+            rjButton1.BorderSize = 5;
+            rjButton1.FlatAppearance.BorderSize = 0;
+            rjButton1.FlatStyle = FlatStyle.Flat;
+            rjButton1.ForeColor = Color.White;
+            rjButton1.Location = new Point(42, 51);
+            rjButton1.Name = "rjButton1";
+            rjButton1.Size = new Size(149, 132);
+            rjButton1.TabIndex = 0;
+            rjButton1.Text = "Usuario";
+            rjButton1.TextColor = Color.White;
+            rjButton1.UseVisualStyleBackColor = false;
             // 
             // Interfaz_Inicio
             // 
@@ -47,11 +68,13 @@
             Controls.Add(panel1);
             Name = "Interfaz_Inicio";
             Text = "Form1";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
+        private CustomControls.RJControls.RJButton rjButton1;
     }
 }
