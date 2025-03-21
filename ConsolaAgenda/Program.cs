ea7a379f79f2399141b0898d0577ad92543b555f
nallely-lopez <lopezgarcializethnallely@gmail.com>
2025-03-20 22:15:16 -0600

    Se implementó en consola las acciones que realizará la aplicación

--- ConsolaAgenda/Program.cs
+++ ConsolaAgenda/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibreriaAgenda;
 
 namespace ConsolaAgenda
 {
@@ -10,6 +11,37 @@ namespace ConsolaAgenda
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(" AGENDA DE TAREAS ");
+                Console.WriteLine("1. Agregar Tarea");
+                Console.WriteLine("2. Mostrar Tareas Pendientes");
+                Console.WriteLine("3. Marcar Tarea Como Completada");
+                Console.WriteLine("4. Salir");
+                Console.Write("Selecciona una opción: ");
+
+                string opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        Agenda.AgregarTarea();
+                        break;
+                    case "2":
+                        Agenda.MostrarTareasPendientes();
+                        break;
+                    case "3":
+                        Agenda.MarcarTareaComoCompletada();
+                        break;
+                    case "4":
+                        Console.WriteLine(" ¡Hasta luego!");
+                        return;
+                    default:
+                        Console.WriteLine(" Opción no válida, intenta de nuevo.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
         }
     }
 }
